e36c1a82613bd2f9c825aa842e895c459a0df58d
Mahesh-100 <maheshkumar.ucen@gmail.com>
2024-01-02 19:39:29 +0530

    Printing Current Date & Time through servlet

---
+++ WebApplication/src/java/DateServlet.java
@@ -0,0 +1,26 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/DateServlet")
+public class DateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		Date date= new Date();
+		out.println("<h2>"+"Current date & Time:"+date.toString()+"<h2>");
+	}
+
+}
