2e961bb2eb278f8042c4edd6d5e53f5cc0b70402
Mahesh-100 <maheshkumar.ucen@gmail.com>
2024-01-03 20:01:19 +0530

    RequestDispatcher

---
+++ WebApplication/src/java/DemoServlet.java
@@ -0,0 +1,37 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DemoServlet
+ */
+@WebServlet("/DemoServlet")
+public class DemoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int result=(int) request.getAttribute("res");
+		response.setContentType("text/html");
+		PrintWriter out= response.getWriter();
+		out.println("<h2>The result is:</h2>"+result);
+	    out.println("<h2>namaste from demoservlet</h2>");
+		
+		
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
