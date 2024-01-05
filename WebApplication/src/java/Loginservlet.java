a9d699ad7ff3a384d6dffb3e146441047580502f
Mahesh-100 <maheshkumar.ucen@gmail.com>
2024-01-05 22:41:03 +0530

    used  session

--- WebApplication/src/java/Loginservlet.java
+++ WebApplication/src/java/Loginservlet.java
@@ -1,51 +1,41 @@
 
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-
-@WebServlet("/Loginservlet")
-public class Loginservlet extends HttpServlet {
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	      private static final String Valid_User="mahesh";
-	      private static final String Valid_Password="mahesh";
+	private static final String Valid_userName="mahesh";
+	private static final String Valid_password="mahesh";
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		response.setContentType("text/html");
+//		PrintWriter out=response.getWriter();
+		String enteredName=request.getParameter("user");
+		String enteredPassword=request.getParameter("user");
+		HttpSession session =request.getSession();
+		session.setAttribute("user", enteredUser);
 		
-		
-		
-		
-		String enteredUser=request.getParameter("user");
-		String enteredpassword=request.getParameter("pwd");
-//		HttpSession session =request.getSession();
-//		session.setAttribute("user", enteredUser);
-		Cookie cookie= new Cookie("user",enteredUser);
-		response.addCookie(cookie);
-		
-		if (isValidUser(enteredUser,enteredpassword)) {
-			response.sendRedirect("http://localhost:8082/WebApplication/DemoServlet");
-		}
-		else {
-		     
+		if(isValidUser(enteredName,enteredPassword)) {
+			response.sendRedirect("http://localhost:8082/WebApplication/ValidServlet");
 			
+		}else {
 			response.sendRedirect("http://localhost:8082/WebApplication/login.html");
+		}
+		
 			
+	}
+	private boolean isValidUser( String username,String password) {
+		return Valid_userName.equals(username)&& Valid_password.equals(password);
 		
 	}
+	
 
 }
-	private boolean isValidUser(String user,String password) {
-		
-		return Valid_User.equals(user)&&Valid_Password.equals(password);
-	}
-
-}
-      
