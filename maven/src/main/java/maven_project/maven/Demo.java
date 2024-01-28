1d25dec18e67608eba6724367dee72eb3f2900c9
Mahesh-100 <maheshkumar.ucen@gmail.com>
2024-01-28 22:32:18 +0530

    created maven project

---
+++ maven/src/main/java/maven_project/maven/Demo.java
@@ -0,0 +1,9 @@
+package maven_project.maven;
+
+public class Demo {
+	public double add(double a,double b) {
+		return a+b;
+		
+	}
+
+}
---
+++ maven/src/test/java/maven_project/maven/AddTest.java
@@ -0,0 +1,7 @@
+package maven_project.maven;
+
+import junit.framework.TestCase;
+
+public class AddTest extends TestCase {
+
+}
